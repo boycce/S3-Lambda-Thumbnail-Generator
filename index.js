f08b84c069a32f86c452eefdbdb9236977028a5e
Ricky Boyce <boycce@gmail.com>
2022-06-17 11:18:55 +1200

    fix: updated package.json, updated zip, added eslint, etc

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-// Node 8.10
-var aws = require("aws-sdk")
-var fs = require("fs")
-var mktemp = require("mktemp")
-var gm = require("gm").subClass({ imageMagick: true })
-var s3 = new aws.S3()
+var aws = require('aws-sdk')
+let fs = require('fs')
+let mktemp = require('mktemp')
+let gm = require('gm').subClass({ imageMagick: true })
+let s3 = new aws.S3()
 
-var settings = {
+let settings = {
   // Allowed filetypes
   filetypes: ['png', 'jpg', 'jpeg',  'bmp', 'tiff', 'gif'],
   // Thumbnail sizes (excluding 'full')
   sizes: [
-    { name: "small", width: null, height: 200 },
-    { name: "medium", width: null, height: 750 },
-    { name: "large", width: null, height: 1600 }
+    { name: 'small', width: null, height: 200 },
+    { name: 'medium', width: null, height: 750 },
+    { name: 'large', width: null, height: 1600 }
   ]
 }
 
 exports.handler = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false
-  var tempFile
-  var processing = true
-  var bucket = event.Records[0].s3.bucket.name
-  var key = decodeURIComponent(event.Records[0].s3.object.key).replace(/\+/g, ' ')
-  var filename = key.replace(/^full\//, '')
-  var fileType = (filename.match(/\.\w+$/)? filename.match(/\.\w+$/)[0] : '').substr(1).toLowerCase()
+  let tempFile
+  let processing = true
+  let bucket = event.Records[0].s3.bucket.name
+  let key = decodeURIComponent(event.Records[0].s3.object.key).replace(/\+/g, ' ')
+  let filename = key.replace(/^full\//, '')
+  let fileType = (filename.match(/\.\w+$/)? filename.match(/\.\w+$/)[0] : '').substr(1).toLowerCase()
 
   console.log('Filename:', filename)
   console.log('File Type:', fileType)
@@ -33,9 +32,9 @@ exports.handler = (event, context, callback) => {
 
   // Catch resizing timeouts here so aws doesn't restart the request 3 times,
   // make this number 1 second less than the lambda timeout
-  var timeout = setTimeout(function() { error('Timed out..') }, 8000)
+  let timeout = setTimeout(function() { error('Timed out..') }, 8000)
 
-  for (var size of settings.sizes) {
+  for (let size of settings.sizes) {
     if (filename.indexOf(size.name + '/') === 0) return error()
   }
   if (fileType === '') {
@@ -47,12 +46,12 @@ exports.handler = (event, context, callback) => {
 
   s3.getObject({ Bucket: bucket, Key: key }, (err, res) => {
     if (err) return error(err)
-    var data = res.Body
+    let data = res.Body
 
     if (fileType === 'gif') {
-      tempFile = mktemp.createFileSync("/tmp/XXXXXXXXXX.gif")
+      tempFile = mktemp.createFileSync('/tmp/XXXXXXXXXX.gif')
       fs.writeFileSync(tempFile, res.Body)
-      data = tempFile + "[0]"
+      data = tempFile + '[0]'
     }
 
     Promise.all(settings.sizes.map((size) => {
@@ -68,20 +67,20 @@ exports.handler = (event, context, callback) => {
 
           console.log('Size: ', size.width, 'Height:', height)
 
-          this.resize(size.width, height).toBuffer("jpg", (err, buffer) => {
+          this.resize(size.width, height).toBuffer('jpg', (err, buffer) => {
             console.log('Error this.resize:', err)
             console.log('Bucket:', bucket)
             console.log('Buffer:', buffer)
 
             if (err) return reject(`Unable to generate images for '${bucket}/${filename}', error: ` + err)
 
-            console.log('Key:', size.name + '/' + filename.replace(/\.\w+$/, ".jpg"))
+            console.log('Key:', size.name + '/' + filename.replace(/\.\w+$/, '.jpg'))
 
             s3.putObject({
               Bucket: bucket,
-              Key: size.name + '/' + filename.replace(/\.\w+$/, ".jpg"),
+              Key: size.name + '/' + filename.replace(/\.\w+$/, '.jpg'),
               Body: buffer,
-              ContentType: "image/jpg",
+              ContentType: 'image/jpg',
               ACL: 'public-read',
               Metadata: { thumbnail: 'TRUE' }
             }, (err) => {
